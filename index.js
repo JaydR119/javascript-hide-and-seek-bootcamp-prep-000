fbbaa2e135cc67ac6b1de10512e56891268908b9
Jayd Robinson <jaydr119@gmail.com>
2019-09-17 11:47:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  var deepest = document.getElementById('#grand-node');
-  deepest.querySelectorAll('div');
-  return deepest[deepest.length - 1];
+  let node = document.getElementById('grand-node');
+  let nextNode = node.children[0];
+  while (nextNode) {
+    node = nextNode;
+    nextNode = node.children[0];
+  }
 }
