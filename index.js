ba169c1216be14a73748300c7032f7eb9933f603
Jayd Robinson <jaydr119@gmail.com>
2019-09-17 11:58:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,23 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  var deepest = document.getElementById("grand-node").lastChild;
-  return deepest;
+  var answer
+  var children = {}
+  var counter = 0
+  var list = document.querySelectorAll('#grand-node > div')
+  for (var i = 0; i < list.length; i++) {
+    var current_child = list[i]
+    while (current_child.querySelector(':first-child') != null) {
+      counter += 1
+      current_child = current_child.querySelector(':first-child')
+      deepestChild = current_child
+    }
+    if (children[i] === undefined || children[i] < counter) {
+      children[i] = counter
+      answer = deepestChild
+    }
+    counter = 0
+    }
+    return answer
+    }
 }
